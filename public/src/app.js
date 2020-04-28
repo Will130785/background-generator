2f12d44f0d5caab0c85213f6d3f4cee007d3a30b
willconstable <will_constable@msn.com>
2020-04-28 10:37:39 +0100

    add functionality for entering custom rgb value

--- public/src/app.js
+++ public/src/app.js
@@ -2,6 +2,10 @@
 const functionality = {
     generateBtn: document.getElementById("generate"),
     colorDisplay: document.querySelector(".display"),
+    rgbForm: document.querySelector(".rgb-input"),
+    rValue: document.querySelector("#r-value"),
+    gValue: document.querySelector("#g-value"),
+    bValue: document.querySelector("#b-value"),
     generateRandom: () => {
         
         //Create random rgb values
@@ -12,8 +16,23 @@ const functionality = {
         //Display in UI
         document.querySelector("body").style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
         functionality.colorDisplay.textContent = `RGB Value: ${r} ${g} ${b}`;
+        functionality.rValue.value = "";
+        functionality.gValue.value = "";
+        functionality.bValue.value = "";
     }
 }
 
 //Event listeners
+//Generate random button
 functionality.generateBtn.addEventListener("click", functionality.generateRandom);
+//Pick rgb value button
+functionality.rgbForm.addEventListener("submit", e => {
+    e.preventDefault();
+    let r = functionality.rValue.value;
+    let g = functionality.gValue.value;
+    let b = functionality.bValue.value;
+
+    //Display in UI
+    document.querySelector("body").style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    functionality.colorDisplay.textContent = `RGB Value: ${r} ${g} ${b}`;
+});
